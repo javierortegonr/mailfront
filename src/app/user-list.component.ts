8cee1ee30165d82a3a2ae35b90a9d844753becd0
Javier Ortegon <javierortegonmp@gmail.com>
2025-09-14 15:32:18 -0500

    feat: full front

---
+++ src/app/user-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserService, User } from './user.service';
+
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+  users: User[] = [];
+  loading = true;
+  error = '';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Error al obtener usuarios';
+        this.loading = false;
+      }
+    });
+  }
+}
